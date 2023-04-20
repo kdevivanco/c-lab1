d4c7e577a15ee8dcde1e7a7e827e863b5955f645
kdevivanco <kdevivanco@gmail.com>
2023-04-20 11:08:16 -0500

    esqueleto sin funcionalidad

--- src/tuit.cpp
+++ src/tuit.cpp
@@ -1,12 +1,52 @@
 //
-// Created by rudri on 10/11/2020.
+// Created by Kayla de Vivanco on 04/10/2023
 //
 
 #include <iostream>
+#include <string>
+#include <vector>
 #include "tuit.h"
+
+using std::string;
+using std::vector;
+using std::ostream;
 using namespace std;
 
+Tuit::Tuit(string _user_name, string _text) {
+    user_name = _user_name;
+    text = _text;
+    return;
+}
 
-void question_1(){
+ostream& operator<<(ostream& out, const Tuit& tuit){
+    out << "===============" << endl;
+    out << tuit.user_name << endl;
+    out << tuit.text << endl;
+    return out;
+}
 
+
+//Media::Media(int id, string file_name){
+//    Element(id), file_name(file_name){ //Element inicializa a id porque es una var privada de Element.
+//    }
+//}
+
+Tuit &Tuit::add_element(Element* element){
+    this->elements.push_back(element);
+    return *this;
+}
+
+//ostream &Media::show(ostream &out){
+//    return out;
+//}
+
+//
+//void question_1(){
+//
+//}
+
+
+int main(){
+    Tuit tuit1("kdevivanco", "hola mundo");
+    cout << tuit1;
 }
--- src/tuit.h
+++ src/tuit.h
@@ -1,6 +1,7 @@
 //
-// Created by rudri on 10/11/2020.
+// Created by Kayla de Vivanco on 04/10/2023
 //
+
 
 #ifndef PROG3_UNIT1_TUIT_V2023_1_TUIT_H
 #define PROG3_UNIT1_TUIT_V2023_1_TUIT_H
@@ -22,55 +23,71 @@ class Tuit {
 
     //Metodos
 public:
-    Tuit& add_element(Element* element); //Retorna Tuit y no void -> para poder llamar metodo sobre lo que retorna
+//    Tuit() = default;
+    Tuit(string _user_name, string _text);
+    //~Tuit();
+
+    Tuit(const Tuit& other);
+    Tuit(Tuit&& other)noexcept;
+    //Copy assignment
+    Tuit& operator=(const Tuit& other);
+    //Move assignment
+    Tuit& operator=(Tuit&& other)noexcept;
+
+    //Metodos
+    Tuit& add_element(Element* element);
+    Tuit& add_reply(Tuit tuit);
+    friend ostream& operator<<(ostream& out, const Tuit& tuit);
+
 
 };
 
-class Element { //Clase Abstracta (linea 38)
-    int id=0; //para crear un constructor por parametros
-    //Un constructor nunca puede ser virtual
-
-public:
-    Element(int id); //Constructor por parametros - no es virtual
 
 
-    //El destructor si debe de colocarse de forma virtual para hacer herencia
-    virtual ~Element() = default; //Ese ~ hace que sea virtual - genera un arreglo de punteros (v table) esto apunta al codigo del destructor
-
-    virtual ostream& show(ostream& out) = 0; //Metodo Abstracto , hasta antes de esto la clase era virtual. Ahora es abstracta
-    // en este caso el vtable apunta a 0/nullptr -> no tiene definiciones
-};
-
-class Media: public Element{ //Declaracion de herencia, hasta aqui tambien es abstracta
-    string file_name;
-
-public:
-    Media(int id,string file_name); //Se debe incluir el constructor de la clase padre. Hereda "obligaciones"
-    //para que deje de ser virtual: sobreescritura (se define el metodo virtual de la clase padre)
-    ostream& show(ostream& out) override; //se elimina el virtual y se escribe override
-};
-
-class Emoji: public Element{
-    string symbol;
-    int size;
-
-public:
-    Emoji(int id,int size,string symbol);
-    ostream& show(ostream& out) override;
-};
-
-class Poll: public Element{
-    string title;
-    vector<string> choices;
-    int days;
-    int hours;
-    int minutes;
-
-public:
-    Poll(int id,string title, vector<string> choices, int days, int hours, int minutes);
-    ostream& show(ostream& out) override;
-};
-
-void question_1();
+//class Element { //Clase Abstracta (linea 38)
+//    int id=0; //para crear un constructor por parametros
+//    //Un constructor nunca puede ser virtual
+//
+//public:
+//    Element(int id); //Constructor por parametros - no es virtual
+//
+//    //El destructor si debe de colocarse de forma virtual para hacer herencia
+//    virtual ~Element() = default; //Ese ~ hace que sea virtual - genera un arreglo de punteros (v table) esto apunta al codigo del destructor
+//
+//    virtual ostream& show(ostream& out) = 0; //Metodo Abstracto , hasta antes de esto la clase era virtual. Ahora es abstracta
+//    // en este caso el vtable apunta a 0/nullptr -> no tiene definiciones
+//};
+//
+//class Media: public Element{ //Declaracion de herencia, hasta aqui tambien es abstracta
+//    string file_name;
+//
+//public:
+//    Media(int id,string file_name); //Se debe incluir el constructor de la clase padre. Hereda "obligaciones"
+//    //para que deje de ser virtual: sobreescritura (se define el metodo virtual de la clase padre)
+//    ostream& show(ostream& out) override; //se elimina el virtual y se escribe override
+//};
+//
+//class Emoji: public Element{
+//    string symbol;
+//    int size;
+//
+//public:
+//    Emoji(int id,int size,string symbol);
+//    ostream& show(ostream& out) override;
+//};
+//
+//class Poll: public Element{
+//    string title;
+//    vector<string> choices;
+//    int days;
+//    int hours;
+//    int minutes;
+//
+//public:
+//    Poll(int id,string title, vector<string> choices, int days, int hours, int minutes);
+//    ostream& show(ostream& out) override;
+//};
+//
+//void question_1();
 
 #endif //PROG3_UNIT1_TUIT_V2023_1_TUIT_H
